c5846402c4c4d9997b16d1309ff51e27304ce582
ACE-OF-DIAMONDS <53719487+mystery-z@users.noreply.github.com>
2022-05-04 17:34:30 +0800

    added start_chat(), get_local(), and getUTC()
    yeet

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::time::SystemTime;
 use chrono::prelude::*;
+use std::io;
+
 
 fn main() {
-    get_time();
+    start_chat();
 }
 
-fn get_time(){
+fn get_UTC(){
     // let _time_now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH);
 
     // match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
@@ -13,6 +15,18 @@ fn get_time(){
     //     Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     // }
     let utc: DateTime<Utc> = Utc::now();
+    println!("{}", utc);
+}
 
-    println!("{}", utc)
+fn get_local(){
+	let localtime = chrono::offset::Local::now();
+	println!("{:?}", localtime);	
+}
+
+fn start_chat(){
+	println!("\n---Starting Chat--- \nThe current time in UTC is: ");
+	get_UTC();
+	
+	println!("\nThe current Local time is: ");
+	get_local();	
 }
