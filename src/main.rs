55c611ae01d1f9b79856f5f77c132b05fba8f7e8
iCraft85 <aryanb.in@gmail.com>
2022-05-04 16:45:02 +0800

    e

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::time::SystemTime;
-
+use chrono::prelude::*;
 
 fn main() {
-    let _time_now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH);
-    // println!("{}", _time_now.as_secs())
+    get_time();
+}
 
-    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(n) => println!("{}", n.as_secs()),
-        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
-    }
+fn get_time(){
+    // let _time_now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH);
+
+    // match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+    //     Ok(n) => println!("{}", n.as_secs()),
+    //     Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    // }
+    let utc: DateTime<Utc> = Utc::now();
+
+    println!("{}", utc)
 }
