b09f4dc2397b766b6aadfc22adf99c80e36f4c3b
iCraft85 <aryanb.in@gmail.com>
2022-05-04 22:09:24 +0800

    e

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,31 @@
 use std::time::SystemTime;
 use chrono::prelude::*;
-use std::io;
 
 
 fn main() {
-    start_chat();
+    get_real_time(get_unix_time());
+
+    // println!("{}", time)
 }
 
-fn get_UTC(){
-    // let _time_now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH);
+fn get_unix_time() -> i64{
+    let _time_now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH);
 
-    // match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-    //     Ok(n) => println!("{}", n.as_secs()),
-    //     Err(_) => panic!("SystemTime before UNIX EPOCH!"),
-    // }
-    let utc: DateTime<Utc> = Utc::now();
-    println!("{}", utc);
+    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(n) => n.as_secs().try_into().unwrap(),
+        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    }
 }
 
-fn get_local(){
-	let localtime = chrono::offset::Local::now();
-	println!("{:?}", localtime);	
-}
+fn get_real_time(_unix_time: i64){
+    let naive = NaiveDateTime::from_timestamp(_unix_time.into(), 0);
+    
+    let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
+    
+    let timeUnstructured = datetime.format("%Y %m %d %H %M %S");
+    let timeFormatted = format!("{}", timeUnstructured);
+    let time = timeFormatted.split(" ").collect::<Vec<_>>();
 
-fn start_chat(){
-	println!("\n---Starting Chat--- \nThe current time in UTC is: ");
-	get_UTC();
-	
-	println!("\nThe current Local time is: ");
-	get_local();	
+
+    println!("{:?}", time);
 }
