9b798a02e5f7b68ffd4be90f8835b31ec6c3581f
ACE-OF-DIAMONDS <53719487+mystery-z@users.noreply.github.com>
2022-07-22 05:22:37 +0900

    added test_ansiEsc.lua for testing ansi esc commands

---
+++ nightly_src/tests_nightly/test_ansiEsc.lua
@@ -0,0 +1,5 @@
+--~ https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+--~ https://stackoverflow.com/questions/7248076/python-how-to-modify-edit-the-string-printed-to-screen-and-read-it-back
+print("\x1b[1;31m" .. "wtf is this actually red and bold")
+  --~  Set style to bold, red foreground.
+
