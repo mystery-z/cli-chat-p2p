00f3b87e3a06e8e2d9d32e8821665b3338d6c844
ACE-OF-DIAMONDS <53719487+mystery-z@users.noreply.github.com>
2022-08-01 01:38:50 +0900

    [fallback] packets working
    minor issue with port being str not int

--- r-src/R_combined.rb
+++ r-src/R_combined.rb
@@ -1,24 +1,25 @@
 require 'socket' 
 require 'thread'
+require 'json'
 
+# [TODO] delete these later
 ip = '192.168.1.11'
 port = 1880
 
-#~ delete here latr
 myip = '192.168.1.11'
 myport = 1880
 
-def listener(myip, myport)
-  #import port and hostname
+def listener(ip, port)
+  #~ open for an ip and port
   open_Socket = TCPSocket.open(ip, port)
-
   loop do
-    # receive msg from socket
+    #~ receive msg from socket
     msg = open_Socket.gets     
     if msg != nil
-      msg = msg.chop()
-      # print the msg
-      puts(msg)  
+      #~ unpack the json file
+      json_unpacker(msg)
+      #~ print the msg
+      puts($parsed_msg)  
     else 
       next
     end
@@ -28,32 +29,57 @@ end
 
 def sender(myip, myport)
   server = TCPServer.new(myip, myport)
-   # somehow works with global ip but requires port forwarding
+  #~ somehow works with global ip but requires port forwarding
   loop do 
     client = server.accept
     loop do         
-    # Servers run forever       
+    #~ Server runs forever       
       print(">>") 
       msg = gets
+      msg = msg.chop()
       json_packer(myip, myport, msg)     
-      #~ make the packed json the sent thing, not msg
-      client.print(msg)       
+      #~ json_packer puts the msg inside the formatted json, MUST use file.READ not file.OPEN
+      json_msg = File.read("send.json")
+      client.print(json_msg)       
+      #~ get the correctly formatted packet and send it (previous TWO lines)
     end
   end
 end
 #~ works
 
 def json_packer(myip, myport, msg)
+  #~ get the time
   time = Time.new
   time = time.inspect
-  json_data = "{'ip': '#{myip}', 'port': #{myport}, 'msg': '#{msg}', 'time':'#{time}'}"
+  #~ set the structure for the json file 
+  json_data = {
+    "ip" => "#{myip}", 
+    "port" => "#{myport}", 
+    "time" => "#{time}", 
+    "msg"=> "#{msg}" 
+  } 
+  # [IMPORTANT, TODO] change myport back to an integer (from string) later
+  #~ write the actual .json file
   file = File.new("send.json", "w")
-  file.syswrite(json_data)
-  file.close()
+  file.syswrite(json_data.to_json+"\n")
+  #~ the close is required because of the File.new operator
+  file.close
 end
 
+def json_unpacker(msg)
+  #~ open a new file and dump the packet in
+  file = File.new("received.json","w")
+  file.syswrite(msg)
+  file.close
+  #~ reopen the json file and parse it (im pretty sure there's a more efficient way but can't think of it)
+  file1 = File.open("received.json")
+  #~ load the json file
+  data = JSON.load(file1)
+  #~ save the actual parsed msg as a global var
+  $parsed_msg = data['msg']
+end
 
-#~ listener(ip, port)
-sender(ip,port)
+listener(myip, port)
+#~ sender(ip,port)
 
 
